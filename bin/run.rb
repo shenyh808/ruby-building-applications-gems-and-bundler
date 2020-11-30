2c038795cebd1c3ec58e44badb359739afa7b7b2
shen huang <shenyh33@gmail.com>
2020-11-30 10:27:22 -1000

    commit tree 3e665d05ed18a6b1bb089f496dbfd143a94e4a43

--- bin/run.rb
+++ bin/run.rb
@@ -1,1 +1,29 @@
 # require the environment file
+# require 'awesome_print'
+
+
+require_relative '../config/environment'
+
+  hash = { :a => 4, :b => 10 }
+ ap hash
+
+# require_relative '../config/environment'
+
+# array = [1,2,3,4]
+# hash = {:a => 1, :b => 5}
+# ap array
+# ap hash 
+
+# [
+#     [0] 1,
+#     [1] 2,
+#     [2] 3,
+#     [3] 4
+# ] 
+
+# {
+#     :a => 1,
+#     :b => 5
+# } 
+
+# require_relative '../config/environment'
